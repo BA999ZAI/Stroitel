31900bb25c2c7e94610d58e63f22bb482da6f0fa
Иван <99138124+BA999ZAI@users.noreply.github.com>
2023-02-15 11:38:12 +0300

    не полный рефакторинг кода

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,15 @@
 import './App.scss';
 import { Routes, Route, Link, Outlet } from 'react-router-dom';
 import { Map, Placemark, YMaps } from '@pbe/react-yandex-maps';
-
+import { useState } from 'react';
+// Изображения
 import logo from './assets/Images/Logo.png';
 import oblakaLeft from './assets/Images/oblakaLeft.png';
 import oblakaRight from './assets/Images/oblakaRight.png';
 import topSmallImage from './assets/Images/1smallImage.png';
 import centerSmallImage from './assets/Images/2smallImage.png';
 import bottomSmallImage from './assets/Images/3smallImage.png';
-
+// Иконки
 import iconKey from './assets/Icons/iconKey.png';
 import iconOffice from './assets/Icons/iconOffice.png';
 import iconBrickwall from './assets/Icons/iconBrickwall.png';
@@ -25,11 +26,10 @@ import iconGilKomp from './assets/Icons/iconGilKomp.png';
 import iconBabyGarden from './assets/Icons/iconBabyGarden.png';
 import iconACS from './assets/Icons/iconACS.png';
 import iconPlus from './assets/Icons/iconPlus.png';
-
+// Компоненты
 import PriceProjects from './components/priceProjects/priceProjects';
 import InfComplecs from './components/infComplecs/infComplecs';
 import FullInformation from './components/fullInformation/fullInformation';
-import { useState } from 'react';
 
 
 export default function App () {
@@ -77,59 +77,59 @@ const Layout = () => {
 
 
   return (
-    <div style={{display: 'flex', width: '100%', flexDirection: 'column'}}>
-      <div className='BackImage' style={{height: '800px', width: '100%', display: 'flex', flexDirection: 'column'}}>
-        <div className='BackWhite' style={{height: '121px', width: '100%', display: 'flex'}}>
-          <div style={{margin: '12px 150px', display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+    <div className='d-flex w-full flex-col'>
+      <div className='BackImage d-flex w-full flex-col' style={{height: '800px'}}>
+        <div className='BackWhite d-flex w-full' style={{height: '121px'}}>
+          <div className='d-flex items-center content-between mx-150 my-12'>
             <img src={logo} alt='' style={{width: '180px', height: '90px'}} />
             
-            <div style={{display: 'flex', justifyContent: 'space-between', margin: 'auto 110px'}}>
+            <div className='d-flex content-between my-auto mx-110'>
               <Link to='/' className='links mr-32'>Жилые комплексы</Link>
               <Link to='/company' className='links mr-32'>О компании</Link>
               <Link to='/sale' className='links mr-32'>Как купить</Link>
               <Link to='/contact' className='links'>Контакты</Link>
             </div>
 
-            <div style={{display: 'flex', flexDirection: 'column', margin: '21px 0px'}}>
+            <div className='d-flex flex-col my-21'>
               <p style={{marginBottom: '7px'}}>+7 (918) 377-48-10</p>
-              <p style={{borderBottom: '1px solid #212D55', margin: '0px auto'}}>Заказать звонок</p>
+              <p className='mx-auto' style={{borderBottom: '1px solid #212D55'}}>Заказать звонок</p>
             </div>
           </div>
         </div>
 
-        <div className='BorderBlue' style={{width: '100%', display: 'flex', justifyContent: 'space-between', paddingTop: '30px', paddingBottom: '30px', marginBottom: '65px'}}>
-          <a style={{marginLeft: '150px'}}>Преимущества</a>
+        <div className='BorderBlue d-flex w-100 content-between py-30 mb-65'>
+          <a className='ml-150'>Преимущества</a>
           <a>Инфраструктура</a>
           <a>Планировка</a>
           <a>Отделка</a>
           <a>Описание</a>
           <a>Контакты</a>
-          <a style={{marginRight: '150px'}}>Документация</a>
+          <a className='mr-150'>Документация</a>
         </div>      
 
-        <p style={{fontSize: '60px', margin: '0px auto 0px auto'}}>Жилой Комплекс <b>«Яблоновский»</b></p>
-        <p style={{fontSize: '32px', margin: '28px auto 0px auto'}}>Краснодар, пгт. Яблоновский ул. Кобцевой</p>
-        <p style={{fontSize: '35px', margin: '0px auto 35px auto'}}><b>от 63 801 до 65 678 ₽/м²</b></p>
+        <p className='mx-auto size-60'>Жилой Комплекс <b>«Яблоновский»</b></p>
+        <p className='mx-auto mt-28 size-32'>Краснодар, пгт. Яблоновский ул. Кобцевой</p>
+        <p className='mx-auto mb-35 size-35'><b>от 63 801 до 65 678 ₽/м²</b></p>
       
         <button type='submit' className='button1'>Узнать стоимость проекта</button>
       
-        <div style={{display: 'flex', height: '100%', justifyContent: 'space-between', margin: '41px 200px 0px 200px', zIndex: '100'}}>
+        <div className='d-flex h-full content-between mt-41 mx-200 zIndex-100'>
           <PriceProjects title='Квартиры' body='от 63 801 ₽/м²' />
           <PriceProjects title='Стоимость' body='от 4,44 млн ₽' />
           <PriceProjects title='Класс' body='Эконом' />
           <PriceProjects title='Срок сдачи' body='Сдан' />
         </div>
 
-        <div style={{display: 'flex', position: 'relative'}}>
-          <img src={oblakaLeft} style={{top: '-180px', position: 'absolute'}} />
-          <img src={oblakaRight} style={{position: 'absolute', right: '0px', top: '-180px'}} />
+        <div className='d-flex pos-relative'>
+          <img src={oblakaLeft} className='topMin180 pos-absolute' />
+          <img src={oblakaRight} className='topMin180 right0 pos-absolute' />
         </div>
         
       </div>
       
-      <div style={{flexDirection: 'column', margin: '90px 150px 100px 150px'}}>
+      <div className='flex-col mx-150 mt-90 mb-100'>
         {/* ВЕРХНИЙ БЛОК */}
-        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        <div className='d-flex content-between'>
           <InfComplecs image={iconKey} title='Срок сдачи' body='2015' />
           <InfComplecs image={iconOffice} title='Этажность' body='10' />
           <InfComplecs image={iconBrickwall} title='Тип дома' body='Кирпичный' />
@@ -138,7 +138,7 @@ const Layout = () => {
         
         
         {/* НИЖНИЙ БЛОК */}
-        <div style={{width: '100%', display: 'flex', justifyContent: 'space-between', marginTop: '30px'}}>
+        <div className='d-flex w-full content-between mt-30'>
           <InfComplecs image={iconBuilding} title='Корпусов' body='2 корпуса' />
           <InfComplecs image={iconParking} title='Парковка' body='Гостевая' />
           <InfComplecs image={iconSlider} title='Территория' body='Дет.площадки' />
@@ -146,12 +146,12 @@ const Layout = () => {
         </div>
       </div>
 
-      <p style={{margin: '0px auto 60px auto', fontSize: '45px', fontFamily: 'Gilroy-Medium'}}>Наша инфраструктура</p>
+      <p className='mx-auto mb-60 size-45'>Наша инфраструктура</p>
       
       {/* YandexMAP */}
-      <div className='yandexMap' style={{width: '100%', height: '666px'}}>
+      <div className='yandexMap w-full' style={{height: '666px'}}>
         <YMaps>
-          <Map style={{width: '100%', height: '100%', position: 'relative'}} defaultState={{ center: [44.991133, 38.935577], zoom: 15.5}}>
+          <Map className='w-full h-full pos-relative' defaultState={{ center: [44.991133, 38.935577], zoom: 15.5}}>
             <Placemark geometry={dateMark} properties={{iconCaption: textMark}} options={{iconColor: '#31BEF4'}} />
             
             <div className='marker' id='Магазины' style={{top: '40px'}} onClick={(e) => serchMarker(e.currentTarget.id)}>
@@ -202,30 +202,29 @@ const Layout = () => {
           </Map> 
         </YMaps>
 
-        <div id='map' style={{width: '100%', height: '100%'}}></div>
       </div>
-      <p style={{fontSize: '45px', margin: '80px auto 30px auto'}}>Описание Жилого Комплекса</p>
+      <p className='size-45 mx-auto mt-80 mb-30'>Описание Жилого Комплекса</p>
 
-      <div style={{display: 'flex', textAlign: 'center', margin: '0px auto 50px auto'}}>
-        <p style={{fontSize: '22px', color: '#656D89'}}>Нажимайте</p>
+      <div className='d-flex text-center mb-50 mx-auto'>
+        <p className='size-22' style={{color: '#656D89'}}>Нажимайте</p>
         <img style={{margin: 'auto 10px auto 7.5px', width: '6px', height: '11px'}} src={iconVectorLeft} />
         <img style={{margin: 'auto 5.5px auto 0px', width: '6px', height: '11px'}} src={iconVectorRight} />
-        <p style={{fontSize: '22px', color: '#656D89'}}>чтобы подробно ознакомится с проектом</p>
+        <p className='size-22' style={{color: '#656D89'}}>чтобы подробно ознакомится с проектом</p>
       </div>
 
-      <div style={{margin: '0px auto 64px 150px'}}>
-        <div style={{display: 'flex'}}>
-          <FullInformation text='Парковка' style={{marginRight: '40px'}} />
-          <FullInformation text='Архитектура' style={{marginRight: '40px'}} />
-          <FullInformation text='Охрана' style={{marginRight: '40px'}} />
+      <div className='mr-auto ml-150 mb-64'>
+        <div className='d-flex'>
+          <FullInformation text='Парковка' />
+          <FullInformation text='Архитектура' />
+          <FullInformation text='Охрана' />
           <FullInformation text='Территория' />
         </div>
 
-        <div style={{display: 'flex', margin: '60px auto 0px auto'}}>
-          <div className="backBigImage" style={{display: 'flex', width: '848px', height: '573px'}}>
-            <div className="textOnImage" style={{display: 'flex', flexDirection: 'column', margin: '140px 283px 54px 27px'}}>
-              <p style={{fontSize: '22px', margin: '25px auto 0px 20px'}}><b>Территория</b></p>
-              <p style={{fontSize: '16px', margin: '30px 20px auto 20px', color: '#656D89'}}>Жилой комплекс "Яблоновский" расположен на ул. Кобцевой Н.С. в пгт. Яблоновский.
+        <div className='d-flex mx-auto mt-60'>
+          <div className="backBigImage d-flex" style={{width: '848px', height: '573px'}}>
+            <div className="textOnImage d-flex flex-col" style={{margin: '140px 283px 54px 27px'}}>
+              <p className='mt-25 mr-auto ml-20 size-22'><b>Территория</b></p>
+              <p className='mt-30 mx-20 mb-auto size-16' style={{color: '#656D89'}}>Жилой комплекс "Яблоновский" расположен на ул. Кобцевой Н.С. в пгт. Яблоновский.
                 За пять минут пешком можно дойти до муниципальной поликлиники, школы и детского садика.
                 <br/><br/>
                 Этот жилой комплекс располагается в непосредственной близости от улицы Гагарина, 
@@ -237,7 +236,7 @@ const Layout = () => {
             </div>
           </div>
 
-          <div style={{display: 'flex', marginLeft: '30px', flexDirection: 'column'}}>
+          <div className='d-flex flex-col ml-30'>
             <img src={topSmallImage} className="smallImages mb-auto" />
             <img src={centerSmallImage} className="smallImages my-auto" />
             <img src={bottomSmallImage} className="smallImages mt-auto" />
@@ -248,32 +247,31 @@ const Layout = () => {
 
 
       <footer>
-        <center><p style={{margin: '50px 250px', fontSize: '40px'}}><b>
+        <center><p className='my-50 mx-250 size-40'><b>
         Узнайте о выгодных предложениях и получите личную консультацию
         </b></p></center>
         
-        <div style={{display: 'flex', margin: '0px 150px 100px 150px', height: '252px'}}>
-          <div style={{height: '100%', textAlign: 'center', borderRight: '1px dashed #31BEF4'}}>
-            <p style={{fontSize: '22px', margin: '63px 25px 0px auto', width: '350px'}}><b>По любым возникающим Вами<br/>вопросов звоните нам напрямую</b></p>
-            <div style={{display: 'flex', alignItems: 'center', margin: '20px 25px auto auto'}}>
-              <img src={iconPhone} style={{}} />
-              <p style={{fontSize: '24px', width: '200px'}}><b>+7 (918) 377-48-10</b></p>
+        <div className='d-flex mx-150 mb-100' style={{height: '252px'}}>
+          <div className='h-full text-center' style={{borderRight: '1px dashed #31BEF4'}}>
+            <p className='size-22 ml-auto mr-25 mt-63 w-350'><b>По любым возникающим Вами<br/>вопросов звоните нам напрямую</b></p>
+            <div className='d-flex items-center mt-20 mr-25 mb-auto ml-auto'>
+              <img src={iconPhone} />
+              <p className='size-24' style={{width: '200px'}}><b>+7 (918) 377-48-10</b></p>
             </div>
           </div>
 
-          <div style={{display: 'flex', textAlign: 'center'}}>
-            <p style={{margin: 'auto 25px', width: '115px'}}><b>Или заполните<br/>форму</b></p>
+          <div className='d-flex text-center'>
+            <p className='my-auto mx-25' style={{width: '115px'}}><b>Или заполните<br/>форму</b></p>
           </div>
           
-          <div style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
-            <div style={{display: 'flex', margin: '0px auto 25px auto', alignItems: 'center'}}>
-              <div style={{width: '7px', height: '7px', marginRight: '5px', borderRadius: '50%', backgroundColor: '#31BEF4'}} />
+          <div className='w-full d-flex flex-col'>
+            <div className='d-flex items-center mb-25 mx-auto'>
+              <div className='border-cycle mr-5' style={{width: '7px', height: '7px', backgroundColor: '#31BEF4'}} />
               <p style={{color: '#31BEF4'}}>Сейчас мы работаем</p>
             </div>
 
-            <form style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
-              <div style={{display: 'flex', margin: '0px auto'}}>
-
+            <form className='w-full d-flex flex-col'>
+              <div className='d-flex mx-auto'>
                 <div className='dateBlock'>
                   <input type='text' className='inputText' placeholder='Ваше имя' />
                 </div>
@@ -281,23 +279,24 @@ const Layout = () => {
                 <div className='dateBlock ml-58'>
                   <input type='text' className='inputText' placeholder='Ваш номер телефона' />
                 </div>
-
               </div>
                 
-              <div style={{display: 'flex', margin: '25px auto 0px auto'}}>
+              <div className='d-flex mt-25 mx-auto'>
                 <div className='dateBlock'>
                   <input type='text' className='inputText' placeholder='Ваш номер телефона' />
                 </div>
               
-                <button className="button2"><p style={{fontSize: '22px', color: 'white', cursor: 'pointer'}}>Отправить заявку</p></button>
+                <button className="button2">
+                  <p className='c-ponit size-22' style={{color: 'white'}}>Отправить заявку</p>
+                </button>
               </div>
-                
             </form>
             
-            <div style={{display: 'flex', alignItems: 'center', margin: '25px auto 0px auto'}}>
+            <div className='d-flex items-center mt-25 mx-auto'>
               <input type='checkbox' id='cb' /><label for='cb'></label>
-              <p style={{fontSize: '14px', marginLeft: '10px'}}>Я принимаю условия</p>
-              <a style={{fontSize: '14px', color: '#31BEF4', marginLeft: '4px'}}> обработки персональных данных</a>
+              
+              <p className='ml-10 size-14'>Я принимаю условия</p>
+              <a className='size-14 ml-4' style={{color: '#31BEF4'}}> обработки персональных данных</a>
             </div>
           </div>
         </div>
